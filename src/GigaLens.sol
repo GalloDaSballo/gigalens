94be847e49027b587292f7ca1564736ad8132138
Entreprenerd <alex@entreprenerd.xyz>
2023-09-05 21:21:10 +0200

    feat: breaks alternatively for simple and complex data

---
+++ src/GigaLens.sol
@@ -0,0 +1,94 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.19;
+
+/// @notice Turn any contract into a "view" like contract meant to be used offChain to perform a sequence of operations and get a return value
+
+contract GigaLens {
+    struct Operation {
+        uint96 theValue;
+        address theTarget;
+        bytes theCalldata;
+    }
+
+    struct TheCheck {
+        address theTarget;
+        bytes theCalldata;
+    }
+
+    function multicall(Operation[] memory operations, TheCheck memory check, bool throwAtEnd)
+        external
+        payable
+        returns (bytes memory)
+    {
+        uint256 length = operations.length;
+        for (uint256 i; i < length;) {
+            _doACall(operations[i]);
+            unchecked {
+                ++i;
+            }
+        }
+
+        // is this bytes or bytes32? // In the case of a balance it's a bytes32
+        // For some reason this get's capped
+        (, bytes memory res) = check.theTarget.staticcall(check.theCalldata);
+        emit DebugBytes("after call", res);
+        uint256 asNumber = abi.decode(res, (uint256));
+
+        uint256 _toCopy;
+
+        address theTarget  = check.theTarget;
+        bytes memory theCalldata = check.theCalldata;
+        bytes memory _returnData;
+        
+        // NOTE: We revert here
+        
+        assembly {
+            let success := staticcall(
+                gas(), // gas
+                theTarget, // recipient
+                add(theCalldata, 0x20), // inloc
+                mload(theCalldata), // inlen
+                0, // outloc
+                0 // outlen
+            )
+            // limit our copy to 256 bytes
+            _toCopy := returndatasize()
+            // Store the length of the copied bytes
+            mstore(_returnData, _toCopy)
+            // copy the bytes from returndata[0:_toCopy]
+            returndatacopy(add(_returnData, 0x20), 0, _toCopy)
+        }
+        
+        if(throwAtEnd){
+            assembly {
+                revert(add(_returnData, 0x20), _returnData) // Pass Bytes with 1 word offset, must be because solidity allocates the bytes even if it's not showing it
+            }
+        }
+
+        return _returnData;
+    }
+
+    function _doACall(Operation memory op) internal {
+        op.theTarget.call{value: op.theValue}(op.theCalldata);
+    }
+
+    event DebugBytes(string, bytes);
+    event Debug(string, uint256);
+
+    function quoteMulticall(Operation[] memory operations, TheCheck memory check)
+        external
+        payable
+        returns (bytes memory)
+    {
+        try this.multicall(operations, check, true) returns (bytes memory) {}
+        catch (bytes memory reason) {
+            emit DebugBytes("caught", reason);
+            uint256 asNumber = abi.decode(reason, (uint256));
+            emit Debug("asNumber", asNumber);
+            assembly {
+                // return(add(reason, 0x20), reason) // Only if return value is a bytes array else this is not good
+            }
+            return reason;
+        }
+    }
+}
--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -2,23 +2,69 @@
 pragma solidity ^0.8.13;
 
 import {Test, console2} from "forge-std/Test.sol";
-import {Counter} from "../src/Counter.sol";
+import {GigaLens} from "../src/GigaLens.sol";
 
-contract CounterTest is Test {
-    Counter public counter;
+contract TestReturnSingleWord {
+    function doIt() external pure returns (uint256) {
+        return 123456789;
+    }
+}
+
+contract TestReturnBytes {
+    function doIt() external pure returns (bytes memory) {
+        uint256[] memory entries = new uint256[](2);
+        entries[0] = 123456789;
+        entries[1] = 123456789;
+        return abi.encode(entries);
+    }
+}
+
+contract GigaLensTest is Test {
+    GigaLens lens;
+    TestReturnSingleWord single;
+    TestReturnBytes complex;
 
     function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
+        lens = new GigaLens();
+        single = new TestReturnSingleWord();
+        complex = new TestReturnBytes();
+
     }
 
-    function test_Increment() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
-    }
+    event DebugBytesTest(bytes);
 
-    function testFuzz_SetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+    function testCanGetBalance() public {
+        bytes memory data = abi.encodeWithSignature("doIt()");
+        GigaLens.TheCheck memory uintBalanceCheck =
+            GigaLens.TheCheck({theTarget: address(single), theCalldata: data});
+
+        GigaLens.Operation[] memory operations = new GigaLens.Operation[](0);
+
+        // == BASE ==//
+
+        bytes memory resVal = lens.quoteMulticall(operations, uintBalanceCheck);
+        emit DebugBytesTest(resVal);
+        uint256 asNumber = abi.decode(resVal, (uint256));
+        console2.log("asNumber", asNumber);
+
+        uint256 secondNumber = abi.decode(lens.multicall(operations, uintBalanceCheck, false), (uint256));
+        console2.log("secondNumber", secondNumber);
+
+        // == COMPLEX == //
+        GigaLens.TheCheck memory bytesBalanceCheck =
+            GigaLens.TheCheck({theTarget: address(complex), theCalldata: data});
+
+        bytes memory fromDirect = complex.doIt();
+        emit DebugBytesTest(fromDirect);
+        console2.log("about to decode");
+        (uint256[] memory asNumber3) = abi.decode(fromDirect, (uint256[]));
+        console2.log("asNumber3", asNumber3[0]);
+
+
+        bytes memory resVal2 = lens.quoteMulticall(operations, bytesBalanceCheck);
+        emit DebugBytesTest(resVal2);
+        (uint256[] memory asNumber2) = abi.decode(resVal2, (uint256[]));
+        console2.log("asNumber2", asNumber2[0]);
+        console2.log("asNumber21", asNumber2[1]);
     }
 }
